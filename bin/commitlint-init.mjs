ee35a795465a8c4769d5964018ce367dd7fc83f9
Hub-yang <18830279823@163.com>
2024-03-03 11:40:45 +0800

    update: add package install check, update mjs

--- bin/commitlint-init.mjs
+++ bin/commitlint-init.mjs
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 
-import process from 'node:process'
 import { exec } from 'node:child_process'
 import { readFile as r, writeFile as w } from 'node:fs/promises'
 import { promisify } from 'node:util'
-import chalk from 'chalk'
+import { existsSync } from 'node:fs'
 
 const e = promisify(exec)
+async function checkPackage(moduleName) {
+  if (!existsSync(`./node_modules/${moduleName}`))
+    await e('pnpm install chalk --save')
+  const module = await import('chalk')
+  return module?.default || module[moduleName]
+}
 
 // eslint-disable-next-line no-console
 const log = console.log
-const RUN_INSTALL = 'pnpm install @commitlint/{cli,config-conventional} husky lint-staged -D'
+const RUN_INSTALL = 'pnpm install @commitlint/{cli,config-conventional} husky lint-staged --save-dev'
 const RUN_HUSKY_INIT = 'npx husky init'
 const CONFIG_COMMITLINT
 = `export default {
@@ -39,45 +44,44 @@ const WRITE_COMMIT_MSG = '#!/bin/sh\n. "$(dirname "$0")/_/husky.sh"\n\npnpm comm
 const WRITE_COMMIT_PRE = `#!/bin/sh\n. "$(dirname "$0")/_/husky.sh"\n\npnpm lint-staged`
 
 async function init() {
-  const a = process.argv[1]
-  if (/huberyyang-commitlintinit/.test(a)) {
-    // install commitlint husky and lint-staged
-    const { stdout, stderr } = await e(RUN_INSTALL)
-    if (stderr)
-      return chalk.red(stderr)
-    log(stdout)
-    log(`${chalk.green('✔')} ${chalk.inverse.bold('install success!')}`)
+  // check installed chalk or not
+  const chalk = await checkPackage('chalk')
+  // install commitlint husky and lint-staged
+  const { stdout, stderr } = await e(RUN_INSTALL)
+  if (stderr)
+    return chalk.red(stderr)
+  log(stdout)
+  log(`${chalk.green('✔')} ${chalk.inverse.bold('install success!')}`)
 
-    // create commitlint.config.js and write in options
-    await w('commitlint.config.js', CONFIG_COMMITLINT)
-    log(`${chalk.green('✔')} ${chalk.inverse.bold('config commitlint success!')}`)
+  // create commitlint.config.js and write in options
+  await w('commitlint.config.js', CONFIG_COMMITLINT)
+  log(`${chalk.green('✔')} ${chalk.inverse.bold('config commitlint success!')}`)
 
-    // run ‘npx husky init’
-    await e(RUN_HUSKY_INIT)
-    // write in
-    await w('.husky/commit-msg', WRITE_COMMIT_MSG)
-    await w('.husky/pre-commit', WRITE_COMMIT_PRE)
-    log(`${chalk.green('✔')} ${chalk.inverse.bold('config husky success!')}`)
+  // run ‘npx husky init’
+  await e(RUN_HUSKY_INIT)
+  // write in
+  await w('.husky/commit-msg', WRITE_COMMIT_MSG)
+  await w('.husky/pre-commit', WRITE_COMMIT_PRE)
+  log(`${chalk.green('✔')} ${chalk.inverse.bold('config husky success!')}`)
 
-    // write in package.json
-    const n = 'package.json'
-    const f = await r(n)
-    const o = JSON.parse(f)
+  // write in package.json
+  const n = 'package.json'
+  const f = await r(n)
+  const o = JSON.parse(f)
     ;(o.scripts ||= {}).commitlint = 'commitlint --edit'
-    o.husky = {
-      hooks: {
-        'pre-commit': 'lint-staged',
-        'commit-msg': 'commitlint --edit $1',
-      },
-    }
-    o['lint-staged'] = {
-      '*': 'eslint . --fix',
-    }
-    await w(n, `${JSON.stringify(o, null, 2)}\n`)
-
-    log(`${chalk.green('✔')} ${chalk.inverse.bold('config package.json success!\n')}`)
-    log('process down!')
+  o.husky = {
+    hooks: {
+      'pre-commit': 'lint-staged',
+      'commit-msg': 'commitlint --edit $1',
+    },
   }
+  o['lint-staged'] = {
+    '*': 'eslint . --fix',
+  }
+  await w(n, `${JSON.stringify(o, null, 2)}\n`)
+
+  log(`${chalk.green('✔')} ${chalk.inverse.bold('config package.json success!\n')}`)
+  log('process down!')
 }
 
 init()
