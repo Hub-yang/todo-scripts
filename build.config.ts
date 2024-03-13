603ec260fb498b7ca9c3faae2c09c49f1f43bcfe
Hub-yang <18830279823@163.com>
2024-03-13 15:39:38 +0800

    update: rewrite build.config.ts

--- build.config.ts
+++ build.config.ts
@@ -1,22 +1,13 @@
+import { basename } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
+import fg from 'fast-glob'
 
 export default defineBuildConfig({
   entries: [
-    {
-      input: './src/scripts/index',
-      name: 'index',
-      outDir: './dist',
-    },
-    {
-      input: './src/scripts/main',
-      name: 'main',
-      outDir: './dist',
-    },
-    {
-      input: './src/scripts/commitlint-init',
-      name: 'commitlint-init',
-      outDir: './dist',
-    },
+    ...fg.sync('src/scripts/*.ts').map(i => ({
+      input: i.slice(0, -3),
+      name: basename(i).slice(0, -3),
+    })),
   ],
   // Generates .d.ts declaration file
   declaration: true,
