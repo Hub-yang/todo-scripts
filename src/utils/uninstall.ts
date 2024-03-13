f91d4fa1b3c1d6bcbee94cc6b77c52289f193bb9
Hub-yang <18830279823@163.com>
2024-03-13 17:45:18 +0800

    perf: add friendly terminal prompt for uninstall

--- src/utils/uninstall.ts
+++ src/utils/uninstall.ts
@@ -1,13 +1,15 @@
 import process from 'node:process'
 import { execaCommand } from 'execa'
-
 import ora from 'ora'
+import { Print } from './print'
 
 export async function uninstallPackages(pkg: string) {
   try {
+    const spinner = ora()
+    Print.getInstance().startWithDots({ prefixText: 'uninstall running', withOra: true, spinner })
     const command = `pnpm uninstall ${pkg}`
     await execaCommand(command)
-    ora().succeed(`succeed to uninstall ${pkg}!`)
+    spinner.succeed(`succeed to uninstall ${pkg}!`)
   }
   catch (error) {
     console.warn(`\nFail to uninstall ${pkg}`)
